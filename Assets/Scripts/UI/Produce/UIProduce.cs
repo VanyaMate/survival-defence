f6d6a31d876942fea44f06b4c5e0e771be2fbbd6
VanyaMate <buhu653@mail.ru>
2024-01-13 14:04:12 +0300

    add ProduceMenu [wip], add components [wip]

---
+++ Assets/Scripts/UI/Produce/UIProduce.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+namespace UI.Produce
+{
+    public class UIProduce : MonoBehaviour
+    {
+    }
+}
---
+++ Assets/Scripts/UI/Produce/UIProduceItem.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+namespace UI.Progress
+{
+    public class UIProduceItem : MonoBehaviour
+    {
+    }
+}
